597c1ace1d4cb11104390a432e169c7e65132609
ferreyrarocio <rocio.ferreyra.aguirrebengoa@mi.unc.edu.ar>
2023-07-13 18:34:13 -0300

    deploy

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import styles from "./NavBar.module.css";
 
 const Navbar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState("");
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+require("dotenv").config();
+const {PORT} = process.env || 3001;
 
 conn.sync({ alter: true }).then(() => {
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Listening on port`, PORT);
 })
 }).catch(error => console.error(error))
